3104e9b708bd6e39f45b9919ab490f5531437093
Javier <javier.rotelli@gmail.com>
2026-07-20 23:24:47 -0300

    debug gui

--- src/DebugGUI.ts
+++ src/DebugGUI.ts
@@ -1,6 +1,26 @@
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 import type { Camera } from "three";
+import * as THREE from "three";
 
-export function setupDebugGUI(camera: Camera) {
-  const gui = new GUI();
+export default class DebugGUI {
+  private gui: GUI;
+  private camera: Camera;
+  private scene: THREE.Scene;
+
+  constructor(camera: Camera, scene: THREE.Scene) {
+    this.camera = camera;
+    this.scene = scene;
+    this.gui = new GUI();
+    const axesHelper = new THREE.AxesHelper(5);
+    this.scene.add(axesHelper);
+  }
+
+  setup() {
+    const cameraFolder = this.gui.addFolder("Camera");
+    cameraFolder.add(this.camera.position, "x", -100, 100).name("Position X");
+    cameraFolder.add(this.camera.position, "y", -100, 100).name("Position Y");
+    cameraFolder.add(this.camera.position, "z", -100, 100).name("Position Z");
+    //cameraFolder.add(this.camera.lookAt, "x", -100, 100).name("LookAt X");
+    cameraFolder.open();
+  }
 }
